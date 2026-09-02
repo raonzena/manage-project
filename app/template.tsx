fcfb3793bdecda21e03aba16188f7d27d9e59ba1
Sulhwa Won <sulhwa.dev@gmail.com>
2026-09-02 14:01:02 +0900

    add: redirect auth page

--- app/template.tsx
+++ app/template.tsx
@@ -1,8 +1,17 @@
+"use client";
+
 import { GNB } from "@/components/gnb";
 import { Navigation } from "@/components/navigation";
+import { usePathname } from "next/navigation";
 import { container, main } from "./template.css";
 
 export default function Template({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+
+  if (pathname === "/login" || pathname === "/sign-up") {
+    return children;
+  }
+
   return (
     <>
       <GNB />
